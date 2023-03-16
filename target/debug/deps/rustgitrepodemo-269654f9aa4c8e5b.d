9db27673cab76dc39550e2e4d5a42d1d85f61850
Rustist Dzineware <dzineware@gmail.com>
2023-03-16 15:02:37 +0300

    New Update for the Hello World.
    This is pretty much the same with minor changes.

---
+++ target/debug/deps/rustgitrepodemo-269654f9aa4c8e5b.d
@@ -0,0 +1,5 @@
+/Users/mac/Documents/GitHub/rustgitrepodemo/target/debug/deps/rustgitrepodemo-269654f9aa4c8e5b.rmeta: src/main.rs
+
+/Users/mac/Documents/GitHub/rustgitrepodemo/target/debug/deps/rustgitrepodemo-269654f9aa4c8e5b.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/rustgitrepodemo-9761eae9f0a2122f.d
@@ -0,0 +1,5 @@
+/Users/mac/Documents/GitHub/rustgitrepodemo/target/debug/deps/rustgitrepodemo-9761eae9f0a2122f.rmeta: src/main.rs
+
+/Users/mac/Documents/GitHub/rustgitrepodemo/target/debug/deps/rustgitrepodemo-9761eae9f0a2122f.d: src/main.rs
+
+src/main.rs:
